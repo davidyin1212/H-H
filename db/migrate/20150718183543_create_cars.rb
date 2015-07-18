951c068f61ac73e8f99aef96c7132bfad2637dce
davidyin1212 <davidyin1212@gmail.com>
2015-07-18 17:05:04 -0400

    first commit

---
+++ db/migrate/20150718183543_create_cars.rb
@@ -0,0 +1,18 @@
+class CreateCars < ActiveRecord::Migration
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.string :make
+      t.string :model
+      t.string :engine
+      t.string :exterior
+      t.string :interior
+      t.string :delivery_kms
+      t.string :stock_num
+      t.decimal :msrp, :precision => 9, :scale => 2
+      t.text :additional_fees
+      t.decimal :base_price, :precision => 9, :scale => 2
+      t.timestamps null: false
+    end
+  end
+end
