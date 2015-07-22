b29dd636e730f70e6b0e2477a344d363cf2b296f
davidyin1212 <davidyin1212@gmail.com>
2015-07-22 00:18:41 -0400

    created new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,21 @@
 Rails.application.routes.draw do
-  get 'car/index'
-
-  post 'car/create'
-
-  get 'car/update'
-
-  get 'car/delete'
-
-  get 'car/show'
-
-  get 'user/index'
-
-  get 'user/create'
-
-  get 'user/update'
-
-  get 'user/delete'
-
-  get 'user/show'
-
-  get 'car_controller/create'
-
-  get 'car_controller/update'
-
-  get 'car_controller/delete'
-
-  get 'car_controller/index'
-
-  get 'car_controller/show'
-
-  get 'user_controller/create'
-
-  get 'user_controller/update'
-
-  get 'user_controller/delete'
-
-  get 'user_controller/index'
-
-  get 'user_controller/show'
 
   devise_for :users
   get 'welcome/index'
 
   root 'welcome#index'
 
+  resources :users, only: [:index, :create, :update, :show, :destroy] do
+    resources :cars, only:[] do
+      member do
+        get '/index' => 'cars#userCars'
+        put '/add' => 'cars#addToUser'
+      end
+    end
+  end
+
+  resources :cars, only: [:index, :create, :update, :show, :destroy] 
+  get '/orders' => 'cars#carsOrdered'
+  get '/current' => 'cars#carsInProgress'
+  get '/shiped' => 'cars#carsShiped'
 end
