4493949832ad35ab0772d7be451016455f130947
davidyin1212 <davidyin1212@gmail.com>
2015-08-31 00:54:40 -0400

    precompile

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CarSales
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
+    config.assets.paths << Rails.root.join('lib', 'assets', 'bower_components')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
