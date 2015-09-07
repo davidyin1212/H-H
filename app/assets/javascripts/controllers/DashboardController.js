8b76795b4116dda0ed2f2d6ee7eaed0e756eb3bb
davidyin1212 <davidyin1212@gmail.com>
2015-09-06 23:54:54 -0400

    added in full user filter functionality

--- app/assets/javascripts/controllers/DashboardController.js
+++ app/assets/javascripts/controllers/DashboardController.js
@@ -41,41 +41,38 @@ angular.module('HH')
     $location.path("/inventory/" + id);
   }
 
-  $scope.filterUsers = function() {
+  $scope.filterUsers = function(userType) {
     for (var i = 0; i < $scope.users.length; i++) {
-      var val = $scope.users[i].cars[a];
-      if (param != 0) {
-        if (param != val[j].status) {
-          $scope.users[i].notMatchingCars[a].push(val[j]);
-          $scope.users[i].cars[a].splice(j, 1);
-          j--;
+      if (userType == 'client') {
+        if ($scope.filterOptions.client == 'Hide') {
+          var val = $scope.users[i].notMatchingCars[0];
+          $scope.users[i].cars[0] = $scope.users[i].cars[0].concat(val);
+          $scope.users[i].notMatchingCars[0].splice(0, val.length);
+          queryCars(queryParam);
+        } else if ($scope.filterOptions.client == 'Show'){
+          var val = $scope.users[i].cars[0];
+          $scope.users[i].notMatchingCars[0] = $scope.users[i].notMatchingCars[0].concat(val);
+          $scope.users[i].cars[0].splice(0, val.length);
+        }
+      } else if (userType == 'employee') {
+        if ($scope.filterOptions.employee == 'Hide') {
+          var val = $scope.users[i].notMatchingCars[1];
+          $scope.users[i].cars[1] = $scope.users[i].cars[1].concat(val);
+          $scope.users[i].notMatchingCars[1].splice(0, val.length);
+          val = $scope.users[i].notMatchingCars[2];
+          $scope.users[i].cars[2] = $scope.users[i].cars[2].concat(val);
+          $scope.users[i].notMatchingCars[2].splice(0, val.length);
+          queryCars(queryParam);
+        } else if ($scope.filterOptions.employee == 'Show'){
+          var val = $scope.users[i].cars[1];
+          $scope.users[i].notMatchingCars[1] = $scope.users[i].notMatchingCars[1].concat(val);
+          $scope.users[i].cars[1].splice(0, val.length);
+          val = $scope.users[i].cars[2];
+          $scope.users[i].notMatchingCars[2] = $scope.users[i].notMatchingCars[2].concat(val);
+          $scope.users[i].cars[2].splice(0, val.length);
         }
       }
-      var val = $scope.users[i].notMatchingCars[a];
-      if (param != 0) {
-        if (param == val[j].status) {
-          $scope.users[i].cars[a].push(val[j]);
-          val.splice(j, 1);
-          j--;
-        }
-      } else {
-        $scope.users[i].cars[a].push(val[j]);
-        val.splice(j, 1);
-        j--;
-      }
     }
-    if ($scope.filterOptions.client == 'Show') {
-
-    } else if ($scope.filterOptions.client == 'Hide'){
-
-    }
-
-    if ($scope.filterOptions.employee == 'Show') {
-
-    } else if ($scope.filterOptions.employee == 'Hide'){
-
-    }
-    queryCars(queryParam);
   }
 
   function setup() {
@@ -132,6 +129,11 @@ angular.module('HH')
   function queryCars(param) {
     for (var i = 0; i < $scope.users.length; i++) {
       for (var a = 0; a < $scope.users[i].cars.length; a++) {
+        if ($scope.filterOptions.client == 'Hide' && a == '0') {
+          break;
+        } else if ($scope.filterOptions.employee == 'Hide' && (a == '1' || a == '2')) {
+          break;
+        }
         for (var j = 0; j < $scope.users[i].cars[a].length; j++) {
           var val = $scope.users[i].cars[a];
           if (param != 0) {
